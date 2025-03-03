acb172cbc565520494585c3f93674ababe122334
Zachary Blickensderfer <zach.blickensderfer@gmail.com>
2025-03-03 14:37:06 -0800

    Initial commit

---
+++ src/Autocorrect.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Autocorrect
+ * <p>
+ * A command-line tool to suggest similar words when given one not in the dictionary.
+ * </p>
+ * @author Zach Blick
+ * @author YOUR NAME HERE
+ */
+public class Autocorrect {
+
+    /**
+     * Constucts an instance of the Autocorrect class.
+     * @param words The dictionary of acceptable words.
+     * @param threshold The maximum number of edits a suggestion can have.
+     */
+    public Autocorrect(String[] words, int threshold) {
+
+    }
+
+    /**
+     * Runs a test from the tester file, AutocorrectTester.
+     * @param typed The (potentially) misspelled word, provided by the user.
+     * @return An array of all dictionary words with an edit distance less than or equal
+     * to threshold, sorted by edit distnace, then sorted alphabetically.
+     */
+    public String[] runTest(String typed) {
+
+        return new String[0];
+    }
+
+
+    /**
+     * Loads a dictionary of words from the provided textfiles in the dictionaries directory.
+     * @param dictionary The name of the textfile, [dictionary].txt, in the dictionaries directory.
+     * @return An array of Strings containing all words in alphabetical order.
+     */
+    private static String[] loadDictionary(String dictionary)  {
+        try {
+            String line;
+            BufferedReader dictReader = new BufferedReader(new FileReader("dictionaries/" + dictionary + ".txt"));
+            line = dictReader.readLine();
+
+            // Update instance variables with test data
+            int n = Integer.parseInt(line);
+            String[] words = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                line = dictReader.readLine();
+                words[i] = line;
+            }
+            return words;
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/AutocorrectTester.java
@@ -0,0 +1,75 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Timeout;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class AutocorrectTester {
+    private Autocorrect studentSolution;
+    private String[] dictionary, matches;
+    private int threshold;
+    private String typed;
+
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void testSmall() {
+        setTestData(0);
+        studentSolution = new Autocorrect(dictionary, threshold);
+        assertArrayEquals(matches, studentSolution.runTest(typed), "Incorrect words returned.");
+    }
+
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void testMed() {
+        setTestData(1);
+        studentSolution = new Autocorrect(dictionary, threshold);
+        assertArrayEquals(matches, studentSolution.runTest(typed), "Incorrect words returned.");
+    }
+
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void testLarger() {
+        setTestData(2);
+        studentSolution = new Autocorrect(dictionary, threshold);
+        assertArrayEquals(matches, studentSolution.runTest(typed), "Incorrect words returned.");
+    }
+
+    private void setTestData(int test) {
+        // Open files
+        try {
+            BufferedReader testReader = new BufferedReader(new FileReader("test_files/" + test + ".txt"));
+            BufferedReader answerReader = new BufferedReader(new FileReader("test_files/" + test + "_answers.txt"));
+
+            typed = testReader.readLine();
+            threshold = Integer.parseInt(testReader.readLine());
+
+            dictionary = loadWords(testReader);
+            matches = loadWords(answerReader);
+
+        } catch (IOException e) {
+            System.out.println("Error opening test file " + test + ".txt");
+            e.printStackTrace();
+        }
+    }
+
+    private String[] loadWords(BufferedReader br) {
+        String line;
+        try {
+            line = br.readLine();
+
+            // Update instance variables with test data
+            int n = Integer.parseInt(line);
+            String[] words = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                line = br.readLine();
+                words[i] = line;
+            }
+            return words;
+        } catch (IOException e) {
+            System.out.println("Error opening test file");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
